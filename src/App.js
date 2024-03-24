0a1892aeed9dec676b245d39c26fda484b4baef5
maruukii <mohamedmagherbi9@gmail.com>
2024-03-24 05:44:16 +0100

    test v1.1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload..
         </p>
         <a
           className="App-link"
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          zuma nani!!!!
         </a>
+        hh
       </header>
     </div>
   );
